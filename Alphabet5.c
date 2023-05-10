039c85bd8f9b55771d43d47c41126f942c4306e2
vishaly57 <131939727+vishaly57@users.noreply.github.com>
2023-05-10 10:24:58 +0530

    alphabet program
    this code is done by dev c.

---
+++ Alphabet5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+       int ch;
+       ch=97;
+       printf("below are characters from a-z:\n");
+       while(ch<=122)
+    
+       {
+           printf("%c\t",ch);
+           ch++;
+           
+		   
+	   }
+	   return 0;
+}
